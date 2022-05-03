7d8a61577acbd8f7beb68f7d4d1b392f7dad5bd0
Evgeny0088 <evgeny.kuznetsov0088@gmail.com>
2022-05-03 16:43:03 +0300

    new modification of feature 2

--- src/tests/newFeature2Test.java
+++ src/tests/newFeature2Test.java
@@ -6,5 +6,6 @@ public class newFeature2Test {
     @Test
     public void printTest(){
         System.out.println("this is another test for feature 2!...");
+        System.out.println("another modification here!...");
     }
 }
