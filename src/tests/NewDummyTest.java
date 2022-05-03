4bd91baf52bd302fbe34bf26c67429a112c13b15
Evgeny0088 <evgeny.kuznetsov0088@gmail.com>
2022-05-03 16:23:26 +0300

    new dummy added

---
+++ src/tests/NewDummyTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.Test;
+
+public class NewDummyTest {
+
+    @Test
+    public void printTest1(){
+        System.out.println("this is a new test!...");
+    }
+
+}
