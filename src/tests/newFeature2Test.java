39bd2bcdb3c112ed372075cd08db09549d2f9aa7
Evgeny0088 <evgeny.kuznetsov0088@gmail.com>
2022-05-03 16:28:39 +0300

    new feature 2 test added

---
+++ src/tests/newFeature2Test.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.junit.Test;
+
+public class newFeature2Test {
+    @Test
+    public void printTest(){
+        System.out.println("this is another test for feature 2!...");
+    }
+}
